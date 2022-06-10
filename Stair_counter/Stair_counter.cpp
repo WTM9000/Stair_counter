4eac7a491ee03f5910abb07eaf294b22381737c7
Смутин Даниил <Dsmutin2003@yandex.ru>
2022-06-10 13:00:43 +0300

    Добавлена проверка существования файла с входнымми данными

--- Stair_counter/Stair_counter.cpp
+++ Stair_counter/Stair_counter.cpp
@@ -1,5 +1,17 @@
+#include <iostream>
+#include <locale>
+#include <stdexcept>
+#include <fstream>
+
+using namespace std;
+
 int inputCheck() 
 {
+	string path_to_input_file;
+	cout << "Введите полное имя файла с входными данными" << std::endl;
+	cin >> path_to_input_file;
+	ifstream input("filename.dat");
+	if (input.bad() == true) throw runtime_error("Отсутствует файл с входными данными");
 	return 0;
 }
 
@@ -10,5 +22,13 @@ int get_count(int prev_level, int n)
 
 int main() 
 {
+	try
+	{
+		inputCheck();
+	}
+	catch (exception& ex)
+	{
+		cout << "Ошибка времени выполнения: " << ex.what() << endl;
+	}
 	return 0;
 }
